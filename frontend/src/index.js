23201747830a3766adc3354f0f2f468bba4d1d28
Prashanth <yvprashanth@gmail.com>
2019-05-26 15:12:17 -0700

    New Frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,21 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import rootReducer from './reducers'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'font-awesome/css/font-awesome.css'
-import 'bootstrap-social/bootstrap-social.css'
+import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer)
-
-ReactDOM.render(<Provider store={store}>
-                    <App />
-                </Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
